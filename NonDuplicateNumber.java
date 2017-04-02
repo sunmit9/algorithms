ec79ca699c6d94c4cf9bf61bd03c67900f1538c9
sunmit9 <sunmitgirme@ymail.com>
2017-04-02 22:21:16 +0530

    code comments

--- NonDuplicateNumber.java
+++ NonDuplicateNumber.java
@@ -23,11 +23,11 @@ public class NonDuplicateNumber {
         System.out.println("Non repeating number = " + singleNonDuplicate(new int[]{0,1,1}));
     }
 
-    public static int singleNonDuplicate(int[] nums) {
+    private static int singleNonDuplicate(int[] nums) {
         return singleNonDuplicate(nums, 0, nums.length-1);
     }
 
-    public static int singleNonDuplicate(int[] nums, int start, int end){
+    private static int singleNonDuplicate(int[] nums, int start, int end){
         if(start == end){
             return nums[start];
         }
@@ -35,15 +35,23 @@ public class NonDuplicateNumber {
         if(center == 0){
             return nums[start];
         } else if(center % 2 == 0){
+            //if the center is even,
+
+            // if center number and next number are the same, the non duplicate number is on the right.
             if(nums[center] == nums[center + 1]){
                 return singleNonDuplicate(nums, center + 1, end);
             }else{
+                // else the number is on the left
                 return singleNonDuplicate(nums, start, center );
             }
         }else{
+            //if the center is odd,
+
+            // if center number and next number are the same, the non duplicate number is on the left.
             if(nums[center] == nums[center + 1]){
                 return singleNonDuplicate(nums, start, center);
             }else{
+                // else the number is on the right
                 return singleNonDuplicate(nums, center + 1, end);
             }
         }
