0f85b208ef056ea1d08868f1ee0db3a853627560
sunmit9 <sunmit.girme@autodesk.com>
2017-05-23 17:02:56 +0530

    initial commit. not working

--- LongestPalindromeSubstring.java
+++ LongestPalindromeSubstring.java
@@ -1,7 +1,66 @@
-package PACKAGE_NAME;
-
 /**
- * Created by girmes on 03/04/17.
+ * Created by sunmit9 on 03/04/17.
+ *
+ * https://leetcode.com/problems/longest-palindromic-substring
+ *
+ * Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+ *
  */
 public class LongestPalindromeSubstring {
+
+    public static void main(String[] args) {
+        System.out.println("\n\nLongest Palindrome substring is : " + longestPalindromeString("avdscbbd"));
+
+    }
+
+    private static String longestPalindromeString(String s){
+        String longest = null;
+        int n = s.length();
+        boolean dp[][] = new boolean[n][n];
+
+        // dp[i][j] denotes that there is a palindrome from S(i) to s(j).
+
+        // i moves from start of the string to the end.
+        for(int i = n-1; i >= 0 ; i--){
+
+            // j moves from end of string to current position of i.
+            for(int j = i; j < n ; j++){
+
+                // if character at i and j are same,
+                // and there is a palindrome from i+1 to j-1
+                // (i+1>=n || j<=0) is just a boundary condition to avoid ArrayIndexOutOfBound.
+                // the (i+1)>(j-1) condition checks a special condition, which avoids calling dp[p][q] such that p>q.
+                // i.e. sub palindrome from character at right to char at left
+                // we always find palindrome from a character from left to right.
+
+                if(i==j){
+                    dp[i][j]=true;
+                }else{
+                    dp[i][j] = (s.charAt(i) == s.charAt(j) && ( i+1>=n || j<=0  || (i+1)>(j-1) || dp[i+1][j-1]));
+                    if(i+1<n && j>0){
+                        System.out.print("\n\ni = " + i + ", j = " + j + ", dp[" + (i+1)+ "][" + (j-1) + "] = " + dp[i+1][j-1]);
+                    }
+                }
+
+                if(dp[i][j] && (longest == null || longest.length() <= j+1-i)){
+                    longest = s.substring(i, j+1);
+                }
+                printMatrix(dp);
+                System.out.println("\nlongest : " + longest);
+            }
+
+        }
+
+        return longest;
+    }
+
+    private static void printMatrix(boolean mat[][]){
+        System.out.println("\n\n");
+        for(int i = 0;i<mat.length;i++){
+            System.out.println("");
+            for(int j=0; j< mat[0].length;j++){
+                System.out.print(" " + (mat[i][j]?1:0) );
+            }
+        }
+    }
 }
--- LongestSubstring.java
+++ LongestSubstring.java
@@ -1,7 +1,72 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 /**
- * Created by girmes on 03/04/17.
+ * Created by sunmit9 on 03/04/17.
+ *
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters
+ *
+ * Given a string, find the length of the longest substring without repeating characters.
+ *
+ * Examples:
+ * Given "abcabcbb", the answer is "abc", which the length is 3.
+ * Given "bbbbb", the answer is "b", with the length of 1.
+ * Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ *
  */
 public class LongestSubstring {
+
+    public static void main(String[] args) {
+        System.out.println("Length of longest substring = " + lengthOfLongestSubstringHashSet("pwwwkew"));
+    }
+
+    public static int[] lengthOfLongestSubstring(String s) {
+
+        // create a new list that will store the length of non-repeating substring from the indexed character in string.
+        int[] lengthList = new int[s.length()];
+        for(int i = 0; i< s.length(); i++){
+            Set<String> charSet = new HashSet<>();
+            for(int j = i; j<s.length() ;j++){
+                if(charSet.contains(String.valueOf(s.charAt(j)))){
+                    break;
+                }
+                charSet.add(String.valueOf(s.charAt(j)));
+                lengthList[i] = lengthList[i]+1;
+            }
+        }
+        return lengthList;
+    }
+
+    public static int lengthOfLongestSubstringOhOfN(String s) {
+
+        // create a new list that will store the length of non-repeating substring from the indexed character in string.
+        Map<Character, Integer> charSet = new HashMap<>();
+        int max=0;
+        for(int i = 0, j=0 ; j < s.length(); ++j){
+            if(charSet.containsKey(s.charAt(j))){
+                i = Math.max(i, charSet.get(s.charAt(j)) + 1);
+            }
+            charSet.put(s.charAt(j), j);
+            max = Math.max(max, j-i+1);
+        }
+        return max;
+    }
+
+
+    public static int lengthOfLongestSubstringHashSet(String s){
+        int max = 0;
+        int i = 0, j = 0;
+        Set<Character> set = new HashSet<>();
+        while (j < s.length()) {
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j++));
+                max = Math.max(max, set.size());
+            } else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return max;
+    }
 }
